135cd9c97f6653a95918309b644cc749883e1c28
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-03-14 19:55:56 -0700

    Update listPostComments handler query

--- internal/handlers/posts/listPostComments.go
+++ internal/handlers/posts/listPostComments.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"VoizyServer/internal/database"
 	models "VoizyServer/internal/models/posts"
+	"VoizyServer/internal/util"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -80,15 +82,33 @@ func listPostComments(postID, limit, page int64) (models.ListCommentsResponse, e
 
 	selectQuery := `
 		SELECT
-			comment_id,
-			post_id,
-			user_id,
-			content_text,
-			created_at,
-			updated_at
-		FROM comments
-		WHERE post_id = ?
-		ORDER BY created_at DESC
+			c.comment_id,
+			c.post_id,
+			c.user_id,
+			c.content_text,
+			c.created_at,
+			c.updated_at,
+			u.username,
+			up.first_name,
+			up.last_name,
+			up.preferred_name,
+			ui.image_url AS profile_picture,
+			GROUP_CONCAT(DISTINCT cr.reaction_type ORDER BY cr.reacted_at SEPARATOR ', ') AS distinct_reactions,
+			COUNT(cr.comment_reaction_id) AS reaction_count
+		FROM comments c
+		LEFT JOIN users u
+			ON c.user_id = u.user_id
+		LEFT JOIN user_profiles up
+			ON u.user_id = up.user_id
+		LEFT JOIN user_images ui
+			ON u.user_id = ui.user_id
+			AND ui.is_profile_pic = 1
+		LEFT JOIN comment_reactions cr
+			ON c.comment_id = cr.comment_id
+		WHERE c.post_id = ?
+		GROUP BY c.comment_id, c.post_id, c.user_id, c.content_text, c.created_at, c.updated_at,
+						 u.username, up.first_name, up.last_name, up.preferred_name, ui.image_url
+		ORDER BY c.created_at DESC
 		LIMIT ? OFFSET ?
 	`
 	rows, err := database.DB.Query(selectQuery, postID, limit, offset)
@@ -98,6 +118,8 @@ func listPostComments(postID, limit, page int64) (models.ListCommentsResponse, e
 	var comments []models.ListComment
 	for rows.Next() {
 		var c models.ListComment
+		var username, firstName, lastName, preferredName, profilePicURL, reactions sql.NullString
+		var reactionCount int64
 		err := rows.Scan(
 			&c.CommentID,
 			&c.PostID,
@@ -105,11 +127,30 @@ func listPostComments(postID, limit, page int64) (models.ListCommentsResponse, e
 			&c.ContentText,
 			&c.CreatedAt,
 			&c.UpdatedAt,
+			&username,
+			&firstName,
+			&lastName,
+			&preferredName,
+			&profilePicURL,
+			&reactions,
+			&reactionCount,
 		)
 		if err != nil {
 			log.Println("Scan rows error: ", err)
 			continue
 		}
+		c.username = util.SqlNullStringToPointer(username)
+		c.firstName = util.SqlNullStringToPointer(firstName)
+		c.lastName = util.SqlNullStringToPointer(lastName)
+		c.preferredName = util.SqlNullStringToPointer(preferredName)
+		c.ProfilePicURL = util.SqlNullStringToPointer(profilePicURL)
+		if reactions.Valid && reactions.String != "" {
+			reactionsSlice := util.SplitAndTrim(reactions.String, ",")
+			c.Reactions = reactionsSlice
+		} else {
+			c.Reactions = []string{}
+		}
+		c.ReactionCount = reactionCount
 		comments = append(comments, c)
 	}
 	if err = rows.Err(); err != nil {
--- internal/models/posts/listPostComments.go
+++ internal/models/posts/listPostComments.go
@@ -3,12 +3,19 @@ package models
 import "time"
 
 type ListComment struct {
-	CommentID   int64     `json:"commentID"`
-	PostID      int64     `json:"postID"`
-	UserID      int64     `json:"userID"`
-	ContentText string    `json:"contentText"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	CommentID   	int64     `json:"commentID"`
+	PostID      	int64     `json:"postID"`
+	UserID      	int64     `json:"userID"`
+	ContentText 	string    `json:"contentText"`
+	CreatedAt   	time.Time `json:"createdAt"`
+	UpdatedAt   	time.Time `json:"updatedAt"`
+	Username 			*string   `json:"username"`
+	FirstName			*string   `json:"firstName"`
+	LastName			*string   `json:"lastName"`
+	PreferredName *string   `json:"preferredName"`
+	ProfilePicURL *string   `json:"profilePicURL"`
+	Reactions			[]string  `json:"reactions"`
+	ReactionCount	int64     `json:"reactionCount"`
 }
 
 type ListCommentsResponse struct {
